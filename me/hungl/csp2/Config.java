e60e6facd08462d03ed6e23664e97612c30785d8
Akshat Deshpande <70533891+hungl6844@users.noreply.github.com>
2022-10-19 21:26:20 +0000

    main app (some errors + cleanup)

---
+++ me/hungl/csp2/Config.java
@@ -0,0 +1,11 @@
+package me.hungl.csp2;
+
+public class Config {
+  public static String id;
+  public static String text;
+  public class data {
+    public static String choice1;
+    public static String choice2;
+    public static String choice3;
+  }
+}
